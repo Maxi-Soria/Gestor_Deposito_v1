0f7e1871a96bacac887b5120f403c10306d3d6b6
Maximiliano Facundo Soria <107376486+Maxi-Soria@users.noreply.github.com>
2024-01-23 06:58:40 -0300

    Agregado de clases (tipoCliente y Client)

---
+++ dominio/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    class Cliente
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string NombreFantasia { get; set; }
+        public int Cuit { get; set; }
+        public string DatosFacturacion { get; set; }
+        public string Direccion { get; set; }
+        public int Id_TipoCliente { get; set; }
+        public int HistorialEncomiendas { get; set; }
+        public int Telefono { get; set; }
+        public string Email { get; set; }
+        public string Aclaracion { get; set; }
+        public Boolean Estado { get; set; }
+    }
+}
